40ebf412fd759f9f6660192ae5cc690a2a5a3620
Jan Andreasen <jan@tved.it>
2022-11-09 20:58:05 +0100

    I think it works....

--- IntelliHouse2000App/Services/MQTT/MQTTService.cs
+++ IntelliHouse2000App/Services/MQTT/MQTTService.cs
@@ -1,6 +1,167 @@
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Connecting;
+using MQTTnet.Client.Disconnecting;
+using MQTTnet.Client.Options;
+using MQTTnet.Client.Receiving;
+
 namespace IntelliHouse2000App.Services;
 
-public class MQTTService : IMQTTService
+public class MqttService : IMQTTService
 {
-    
+    private static MqttService Instance { get; set; }
+    public static MqttService MqttClient => Instance ??= new MqttService();
+    private IMqttClient _mqttClient;
+    private IMqttClientOptions _mqttClientOptions;
+
+    public event EventHandler<MqttClientConnectedEventArgs> Connected;
+    public event EventHandler<MqttClientDisconnectedEventArgs> Disconnected;
+    public event EventHandler<MqttApplicationMessageReceivedEventArgs> MessageReceived;
+
+    public bool IsConnected()
+    {
+        return _mqttClient.IsConnected;
+    }
+
+    public async Task<bool> Connect()
+    {
+        try
+        {
+            await _mqttClient.ConnectAsync(_mqttClientOptions);
+        }
+        catch (Exception ex)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    public async Task<bool> Reconnect()
+    {
+        try
+        {
+            await _mqttClient.ReconnectAsync();
+        }
+        catch (Exception ex)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    public async Task<bool> Disconnect()
+    {
+        try
+        {
+            await _mqttClient.DisconnectAsync();
+        }
+        catch (Exception ex)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    public void Initialize(IMqttClientOptions mqttClientOptions)
+    {
+        try
+        {
+            _mqttClientOptions = mqttClientOptions;
+            var factory = new MqttFactory();
+            _mqttClient = factory.CreateMqttClient();
+            _mqttClient.ConnectedHandler = new MqttClientConnectedHandlerDelegate(mqttClientConnectedEventArgs =>
+            {
+                Connected?.Invoke(this, mqttClientConnectedEventArgs);
+            });
+            _mqttClient.DisconnectedHandler = new MqttClientDisconnectedHandlerDelegate(disconnectEventArgs =>
+            {
+                Disconnected?.Invoke(this, disconnectEventArgs);
+            });
+            _mqttClient.ApplicationMessageReceivedHandler =
+                new MqttApplicationMessageReceivedHandlerDelegate(messageReceivedArgs =>
+                {
+                    MessageReceived?.Invoke(this, messageReceivedArgs);
+                });
+        }
+        catch (Exception ex)
+        {
+            // Blergh
+        }
+    }
+
+    public async Task<bool> Subscribe(string topic)
+    {
+        try
+        {
+            await _mqttClient.SubscribeAsync(topic);
+        }
+        catch (Exception ex)
+        {
+            return false;
+        }
+
+        return true;
+    }
+
+    public async Task<bool> Publish(MqttApplicationMessage message)
+    {
+        try
+        {
+            await _mqttClient.PublishAsync(message);
+        }
+        catch (Exception ex)
+        {
+            return false;
+        }
+
+        return true;
+    }
+}
+
+internal class MqttConnectedHandler : IMqttClientConnectedHandler
+{
+    Action<MqttClientConnectedEventArgs> _connectedAction;
+
+    public MqttConnectedHandler(Action<MqttClientConnectedEventArgs> connectedAction)
+    {
+        _connectedAction = connectedAction;
+    }
+
+    public async Task HandleConnectedAsync(MqttClientConnectedEventArgs eventArgs)
+    {
+        _connectedAction?.Invoke(eventArgs);
+    }
+}
+
+internal class MqttDisconnectedHandler : IMqttClientDisconnectedHandler
+{
+    Action<MqttClientDisconnectedEventArgs> _disconnectedAction;
+
+    public MqttDisconnectedHandler(Action<MqttClientDisconnectedEventArgs> disconnectAction)
+    {
+        _disconnectedAction = disconnectAction;
+    }
+
+    public async Task HandleDisconnectedAsync(MqttClientDisconnectedEventArgs eventArgs)
+    {
+        _disconnectedAction?.Invoke(eventArgs);
+    }
+}
+
+internal class MqttMessageReceivedHandler : IMqttApplicationMessageReceivedHandler
+{
+    Action<MqttApplicationMessageReceivedEventArgs> _messageReceived;
+
+    public MqttMessageReceivedHandler(Action<MqttApplicationMessageReceivedEventArgs> messageReceived)
+    {
+        _messageReceived = messageReceived;
+    }
+
+    public async Task HandleApplicationMessageReceivedAsync(MqttApplicationMessageReceivedEventArgs eventArgs)
+    {
+        _messageReceived?.Invoke(eventArgs);
+    }
 }
--- IntelliHouse2000App/Views/Home/MainPage.xaml.cs
+++ IntelliHouse2000App/Views/Home/MainPage.xaml.cs
@@ -1,4 +1,10 @@
-﻿using IntelliHouse2000App.Helpers;
+﻿using System.Text;
+using IntelliHouse2000App.Helpers;
+using IntelliHouse2000App.Services;
+using MQTTnet;
+using MQTTnet.Client.Connecting;
+using MQTTnet.Client.Disconnecting;
+using MQTTnet.Client.Options;
 
 namespace IntelliHouse2000App.Views;
 
@@ -10,18 +16,54 @@ public partial class MainPage : ContentPage
 	public MainPage()
 	{
 		InitializeComponent();
+		Connect();
+	}
+	
+	private async void Connect()
+	{
+		MqttService.MqttClient.Initialize(new MqttClientOptionsBuilder().WithClientId("MQTT_APP")
+			.WithCleanSession(true)
+			.WithTcpServer(Constants.MqttBaseUrl)
+			.Build());
+		MqttService.MqttClient.Connected += MqttClient_Connected;
+		MqttService.MqttClient.MessageReceived += MqttClient_MessageReceived;
+		MqttService.MqttClient.Disconnected += MqttClient_Disconnected;
+		await MqttService.MqttClient.Subscribe($"{Constants.MqttBaseUrl}home/system/log");
+		await MqttService.MqttClient.Connect();
+	}
+	
+	private void MqttClient_MessageReceived(object sender, MqttApplicationMessageReceivedEventArgs e)
+	{
+		var topic = e.ApplicationMessage.Topic;
+		var payload = e.ApplicationMessage.ConvertPayloadToString();
+		DisplayAlert(topic, payload, "Ok");
+	}
+	
+	private void MqttClient_Connected(object sender, MqttClientConnectedEventArgs e)
+	{
+		int a;
+		if (e.ConnectResult.ResultCode == MqttClientConnectResultCode.Success)
+			// yay!
+			a = 1;
+		else
+			// bummer ...
+			a = 0;
+	}
+	
+	private async void MqttClient_Disconnected(object sender, MqttClientDisconnectedEventArgs e)
+	{
+			await MqttService.MqttClient.Reconnect();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
+	private async void Publish(object sender, EventArgs e)
 	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
+		var message = await DisplayPromptAsync("Publish Message", "0 to disarm, 1 to partial and 2 to full arm");
+		if (!string.IsNullOrWhiteSpace(message))
+			await MqttService.MqttClient.Publish(new MqttApplicationMessage
+			{
+				Topic = Constants.MqttBaseUrl + "alarm/arm",
+				Payload = Encoding.UTF8.GetBytes(message)
+			});
 	}
 }
 
