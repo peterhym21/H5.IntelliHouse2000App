0034f817afd277757e0db676cb4fc58088f2256d
Peter Hymøller <peterhym21@gmail.com>
2022-11-10 12:30:22 +0100

    Added model

---
+++ IntelliHouse2000App/Models/Climate/ClimateViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntelliHouse2000App.Models.Climate
+{
+    internal class ClimateViewModel 
+    {
+    }
+}
