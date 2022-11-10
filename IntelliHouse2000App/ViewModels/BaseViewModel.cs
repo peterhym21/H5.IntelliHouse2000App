d247f16c972bd7a0d8790040ae7c92b813eff81b
Peter Hymøller <peterhym21@gmail.com>
2022-11-10 12:42:12 +0100

    Added Base ViewModel

---
+++ IntelliHouse2000App/ViewModels/BaseViewModel.cs
@@ -0,0 +1,22 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IntelliHouse2000App.ViewModels
+{
+    public partial class BaseViewModel : ObservableObject
+    {
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(IsNotBusy))]
+        private bool isBusy = false;
+
+        [ObservableProperty]
+        private string title;
+
+        public bool IsNotBusy => !IsBusy;
+
+    }
+}
