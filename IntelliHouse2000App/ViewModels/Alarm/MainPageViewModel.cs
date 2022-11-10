6761727bab9b199fa3c5f1ae29f2f93b4a6f74f1
Nicolai Brock Heuck <nico061c@elevcampus.dk>
2022-11-10 12:30:39 +0100

    Added empty commands to page

--- IntelliHouse2000App/ViewModels/Alarm/MainPageViewModel.cs
+++ IntelliHouse2000App/ViewModels/Alarm/MainPageViewModel.cs
@@ -1,6 +1,31 @@
-﻿namespace IntelliHouse2000App.ViewModels;
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
 
-public class MainPageViewModel
+namespace IntelliHouse2000App.ViewModels;
+
+public partial class MainPageViewModel : ObservableObject
 {
+    public MainPageViewModel()
+    {
+        
+    }
     
+    [RelayCommand]
+    public void Disarm()
+    {
+        Shell.Current.DisplayAlert("Test", "Imagine the alarm was disarmed", "cancel");
+        
+    }
+    
+    [RelayCommand]
+    public void PartiallyArm()
+    {
+        Shell.Current.DisplayAlert("Test", "Imagine the alarm was partially armed", "cancel");
+    }
+    
+    [RelayCommand]
+    public void FullyArm()
+    {
+        Shell.Current.DisplayAlert("Test", "Imagine the alarm was fully armed", "cancel");
+    }
 }
