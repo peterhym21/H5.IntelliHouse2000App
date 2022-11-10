e1f2ff76b8f4beed5ceef535b09ebb3a85322edc
Nicolai Brock Heuck <nico061c@elevcampus.dk>
2022-11-10 10:31:35 +0100

    Added Empty viewModel

--- IntelliHouse2000App/Views/Alarm/MainPage.xaml.cs
+++ IntelliHouse2000App/Views/Alarm/MainPage.xaml.cs
@@ -1,27 +1,16 @@
 ﻿using IntelliHouse2000App.Helpers;
+using IntelliHouse2000App.ViewModels;
 
 namespace IntelliHouse2000App.Views;
 
 [LifeTime(ServiceLifetime.Singleton)]
 public partial class MainPage : ContentPage
 {
-	int count = 0;
-
-	public MainPage()
+	public MainPage(MainPageViewModel vm)
 	{
 		InitializeComponent();
+		BindingContext = vm;
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
 }
 
