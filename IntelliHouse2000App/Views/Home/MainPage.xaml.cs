50848ddad773d97a1631ff484c4220949affd592
Jan Andreasen <jan@tved.it>
2022-11-10 10:18:01 +0100

    WIP

--- IntelliHouse2000App/Views/Home/MainPage.xaml.cs
+++ IntelliHouse2000App/Views/Home/MainPage.xaml.cs
@@ -1,10 +1,4 @@
-﻿using System.Text;
-using IntelliHouse2000App.Helpers;
-using IntelliHouse2000App.Services;
-using MQTTnet;
-using MQTTnet.Client.Connecting;
-using MQTTnet.Client.Disconnecting;
-using MQTTnet.Client.Options;
+﻿using IntelliHouse2000App.Helpers;
 
 namespace IntelliHouse2000App.Views;
 
@@ -12,30 +6,10 @@ namespace IntelliHouse2000App.Views;
 public partial class MainPage : ContentPage
 {
 	int count = 0;
-	private IMQTTService _service;
 
-	public MainPage(IMQTTService service)
+	public MainPage()
 	{
-		_service = service;
 		InitializeComponent();
-	}
-
-	private void MqttClient_MessageReceived(object sender, MqttApplicationMessageReceivedEventArgs e)
-	{
-		var topic = e.ApplicationMessage.Topic;
-		var payload = e.ApplicationMessage.ConvertPayloadToString();
-		DisplayAlert(topic, payload, "Ok");
-	}
-
-	private async void Publish(object sender, EventArgs e)
-	{
-		var message = await DisplayPromptAsync("Publish Message", "0 to disarm, 1 to partial and 2 to full arm");
-		if (!string.IsNullOrWhiteSpace(message))
-			await _service.Publish(new MqttApplicationMessage
-			{
-				Topic = "home/alarm/arm",
-				Payload = Encoding.UTF8.GetBytes(message)
-			});
 	}
 }
 
