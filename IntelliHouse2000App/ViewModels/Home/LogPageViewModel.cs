7aa117eb626e1198bfd3bea54e8e035a3fa9688d
Jan Andreasen <jan@tved.it>
2022-11-10 12:31:25 +0100

    WIP

--- IntelliHouse2000App/ViewModels/Home/LogPageViewModel.cs
+++ IntelliHouse2000App/ViewModels/Home/LogPageViewModel.cs
@@ -1,8 +1,48 @@
+using System.Collections.ObjectModel;
 using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using IntelliHouse2000App.Models;
+using IntelliHouse2000App.Repository;
 
 namespace IntelliHouse2000App.ViewModels.Home;
 
 public class LogPageViewModel : ObservableObject
 {
-    
+    public ObservableCollection<LogMessage> LogMessages { get; set; } = new ObservableCollection<LogMessage>
+    {
+        new LogMessage
+        {
+            Id = 1,
+            Client = "Test",
+            Message = "Test besked",
+            Retain = true,
+            Timestamp = DateTime.Now,
+            Topic = "Something something",
+            QoS = 1
+        },
+        new LogMessage
+        {
+            Id = 2,
+            Client = "Tes2",
+            Message = "Test besked",
+            Retain = false,
+            Timestamp = DateTime.Now,
+            Topic = "Something something darkside",
+            QoS = 0
+        }
+    };
+
+    private readonly IGenericRepository _repository;
+
+    public LogPageViewModel(IGenericRepository repository)
+    {
+        _repository = repository;
+    }
+
+    [RelayCommand]
+    public async void GetCriticalLogsAsync()
+    {
+        var something = await _repository.GetAsync<List<LogMessage>>(new Uri(Constants.ApiBaseUrl));
+        LogMessages = new ObservableCollection<LogMessage>(something);
+    }
 }
