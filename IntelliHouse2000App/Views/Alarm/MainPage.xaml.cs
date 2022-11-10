963d4a15bff356c267d24eb23291f36796695bb5
Nicolai Brock Heuck <nico061c@elevcampus.dk>
2022-11-10 14:21:57 +0100

    Implemented MessagingCenter

--- IntelliHouse2000App/Views/Alarm/MainPage.xaml.cs
+++ IntelliHouse2000App/Views/Alarm/MainPage.xaml.cs
@@ -10,6 +10,10 @@ public partial class MainPage : ContentPage
 	{
 		InitializeComponent();
 		BindingContext = vm;
+		
+		MessagingCenter.Subscribe<MainPageViewModel>(this, Constants.AlarmArmedSubject, _ => DisplayAlert("Alarm", "Alarm has been armed", "Ok", "Cancel"));
+		MessagingCenter.Subscribe<MainPageViewModel>(this, Constants.AlarmPartiallyArmedSubject, _ => DisplayAlert("Alarm", "Alarm has been partially armed", "Ok", "Cancel"));
+		MessagingCenter.Subscribe<MainPageViewModel>(this, Constants.AlarmFullyArmedSubject, _ => DisplayAlert("Alarm", "Alarm has been fully armed", "Ok", "Cancel"));
 	}
 }
 
