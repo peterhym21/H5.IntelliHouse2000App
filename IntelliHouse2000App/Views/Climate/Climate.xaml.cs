f148bd6f320c0e86970ddd6cec95a25797015298
Peter Hymøller <peterhym21@gmail.com>
2022-11-10 08:39:52 +0100

    New Views

--- IntelliHouse2000App/Views/Climate/Climate.xaml.cs
+++
@@ -1,9 +0,0 @@
-namespace IntelliHouse2000App.Views;
-
-public partial class Climate : ContentPage
-{
-	public Climate()
-	{
-		InitializeComponent();
-	}
-}
---
+++ IntelliHouse2000App/Views/Climate/ClimateLivingroom.xaml.cs
@@ -0,0 +1,9 @@
+namespace IntelliHouse2000App.Views;
+
+public partial class ClimateLivingroom : ContentPage
+{
+	public ClimateLivingroom()
+	{
+		InitializeComponent();
+	}
+}
