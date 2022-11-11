f1d88c1f39c55fe0b05409c5c88b2c479007e28f
Jan Andreasen <jan@tved.it>
2022-11-11 12:55:22 +0100

    It builds :)

--- IntelliHouse2000App/Views/Climate/ClimateGraphsPage.xaml.cs
+++ IntelliHouse2000App/Views/Climate/ClimateGraphsPage.xaml.cs
@@ -1,11 +1,4 @@
-<<<<<<< HEAD
-﻿namespace IntelliHouse2000App.Views;
-=======
-﻿using IntelliHouse2000App.ViewModels;
-
 namespace IntelliHouse2000App.Views;
->>>>>>> develop
-
 
 public partial class ClimateGraphsPage : Shell
 {
