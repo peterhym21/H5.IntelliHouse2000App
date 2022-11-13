419d0996bc6e1d107c98aae1acdd4db6cf17ff7b
Nicolai Brock Heuck <nico061c@elevcampus.dk>
2022-11-13 14:28:44 +0100

    Moved Connectivity check into BaseViewModel

--- IntelliHouse2000App/ViewModels/Alarm/MainPageViewModel.cs
+++ IntelliHouse2000App/ViewModels/Alarm/MainPageViewModel.cs
@@ -1,41 +1,16 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
 using CommunityToolkit.Mvvm.Input;
 using IntelliHouse2000App.Services;
+using Microsoft.Maui.Controls.Internals;
 
 namespace IntelliHouse2000App.ViewModels;
 
-public partial class MainPageViewModel : ObservableObject
+public partial class MainPageViewModel : BaseViewModel
 {
     private readonly IAlarmService _alarmService;
-    private readonly IMQTTService _mqttService;
-    private readonly IConnectivity _connectivity;
-    public MainPageViewModel(IAlarmService alarmService, IMQTTService mqttService, IConnectivity connectivity)
+    public MainPageViewModel(IAlarmService alarmService)
     {
         _alarmService = alarmService;
-        _mqttService = mqttService;
-        _connectivity = connectivity;
-        _internetAccess = HasInternetConnection();
-
-        _connectivity.ConnectivityChanged += (sender, args) =>
-        {
-            _internetAccess = args.NetworkAccess == NetworkAccess.Internet && mqttService.IsConnected();
-        };
-        
-        MessagingCenter.Subscribe<MqttService>(this, Constants.MqttDisconnectedSubject, service =>
-        {
-            _internetAccess = HasInternetConnection();
-        });
-        MessagingCenter.Subscribe<MqttService>(this, Constants.MqttConnectedSubject, service =>
-        {
-            _internetAccess = HasInternetConnection();
-        });
-    }
-
-    [ObservableProperty]
-    private bool _internetAccess = false;
-    private bool HasInternetConnection()
-    {
-        return _mqttService.IsConnected() && _connectivity.NetworkAccess == NetworkAccess.Internet;
     }
     
     [RelayCommand]
--- IntelliHouse2000App/ViewModels/BaseViewModel.cs
+++ IntelliHouse2000App/ViewModels/BaseViewModel.cs
@@ -1,9 +1,51 @@
 ﻿using CommunityToolkit.Mvvm.ComponentModel;
+using IntelliHouse2000App.Services;
 
 namespace IntelliHouse2000App.ViewModels
 {
     public partial class BaseViewModel : ObservableObject
     {
+        private readonly IMQTTService _mqttService;
+        private readonly IConnectivity _connectivity;
+        protected BaseViewModel()
+        {
+            _mqttService = DependencyService.Get<IMQTTService>();
+            _connectivity = DependencyService.Get<IConnectivity>(); //NH_TODO: Test if this works
+            
+            HasInternetAccess = HasInternetConnection();
+            HasMQTTAccess = IsConnectedToMQTT();
+
+            _connectivity.ConnectivityChanged += (sender, args) =>
+            {
+                HasInternetAccess = args.NetworkAccess == NetworkAccess.Internet && _mqttService.IsConnected();
+                
+                if (args.NetworkAccess == NetworkAccess.Internet) _mqttService.Connect();
+            };
+        
+            MessagingCenter.Subscribe<MqttService>(this, Constants.MqttDisconnectedSubject, service =>
+            {
+                HasInternetAccess = HasInternetConnection();
+            });
+            MessagingCenter.Subscribe<MqttService>(this, Constants.MqttConnectedSubject, service =>
+            {
+                HasInternetAccess = HasInternetConnection();
+            });
+        }
+
+        [ObservableProperty]
+        private bool _hasInternetAccess;
+        private bool HasInternetConnection()
+        {
+            return _connectivity.NetworkAccess == NetworkAccess.Internet;
+        }
+    
+        [ObservableProperty]
+        private bool _hasMQTTAccess;
+        private bool IsConnectedToMQTT()
+        {
+            return _mqttService.IsConnected();
+        }
+        
         [ObservableProperty]
         [NotifyPropertyChangedFor(nameof(IsNotBusy))]
         private bool isBusy = false;
