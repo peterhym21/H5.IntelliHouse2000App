b85af220338d1440737f0744f6f3a9c094bb3d36
nicolai heuck <nicolaiheuck@gmail.com>
2022-11-13 14:57:12 +0100

    Created ConnectivityService and implemented it in baseViewModel

---
+++ IntelliHouse2000App/Services/Connectivity/IConnectivityService.cs
@@ -0,0 +1,8 @@
+﻿using IntelliHouse2000App.ViewModels;
+
+namespace IntelliHouse2000App.Services.Connectivity;
+
+public interface IConnectivityService
+{
+    void Init(BaseViewModel viewModel);
+}
